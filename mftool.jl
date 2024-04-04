800d5b2ed1f51b4eba77b0e8fca24f5a7115cd97
Ophir Neto <ophir@ufu.br>
2024-04-04 19:20:14 -0300

    Tidying up command-line interface.

--- mftool.jl
+++ mftool.jl
@@ -1,16 +1,15 @@
+#julia
+
 using MFSimUtilities
 using Plots
 using Match
 
-function probePlot(probe)
-    pu = plot(probe.df[:,"t"], probe.df[:,"u"], title = "U")
-    pt = plot(probe.df[:,"t"], probe.df[:,"temp"], title = "Temp")
-    display(plot(pu, pt, layout = (1,2), legend = false))
-end
+function print_instructions(message)
 
-function print_instructions()
+"MFTool - A MFSim case management tool by Ophir Neto
+Release version 04/04/2024 
 
-"MFTool - A MFSim case management tool by Ophir Neto - Release version 04/04/2024 
+$(message)
 
 Usage:
 
@@ -18,10 +17,31 @@ Usage:
 
 Available commands:
 
-plot_probe <probe_path> <update_interval>
-export_case <case_output_path> <path_to_tarball> <number_of_hdf5s> <include first and second hdf5?>
+- plot_probe <probe_path> <update_interval>
+
+- export_case <case_output_path> <path_to_tarball> <number_of_hdf5s> <include first and second hdf5?>
 " |> print
 
+end
+
+function run_command(command_func, args)
+    try 
+        command_func(args)
+    catch e
+        if typeof(e) == BoundsError
+            print_instructions("ERROR: Possible lack of arguments for command \nFull report: $e")
+        else
+            print_instructions("ERROR: Issue running command \nFull report: $e")
+        end
+    end
+end
+
+##########################################################################################################################
+
+function probePlot(probe)
+    pu = plot(probe.df[:,"t"], probe.df[:,"u"], title = "U")
+    pt = plot(probe.df[:,"t"], probe.df[:,"temp"], title = "Temp")
+    display(plot(pu, pt, layout = (1,2), legend = false))
 end
 
 function plot_probe(args)
@@ -37,10 +57,16 @@ function export_case(args)
     export_case_output(args[1], args[2], nhdf5s, include_header)
 end
 
-@match ARGS[1] begin
-    "plot_probe"  => plot_probe(ARGS[2:end])
-    "export_case" => export_case(ARGS[2:end])
-    _             => print_instructions()
-end
+##########################################################################################################################
 
-#function_call()
+if length(ARGS) == 0
+    print_instructions("")
+    return
+else
+    @match ARGS[1] begin
+        "plot_probe"  => run_command(plot_probe, ARGS[2:end])
+        "export_case" => run_command(export_case, ARGS[2:end])
+        "help"        => print_instructions("")
+        _             => print_instructions("ERROR: Unrecognized command")
+    end
+end
